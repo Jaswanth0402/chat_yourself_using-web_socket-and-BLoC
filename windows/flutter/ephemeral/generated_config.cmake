4e29e811d108134383893e03990f05fc65a1cb55
Jaswanth Kumar.J.G <133202551+Jaswanth0402@users.noreply.github.com>
2023-10-20 11:40:35 +0530

    Add files via upload

---
+++ windows/flutter/ephemeral/generated_config.cmake
@@ -0,0 +1,24 @@
+# Generated code do not commit.
+file(TO_CMAKE_PATH "C:\\flutter" FLUTTER_ROOT)
+file(TO_CMAKE_PATH "D:\\flutter\\web_socket" PROJECT_DIR)
+
+set(FLUTTER_VERSION "1.0.0+1" PARENT_SCOPE)
+set(FLUTTER_VERSION_MAJOR 1 PARENT_SCOPE)
+set(FLUTTER_VERSION_MINOR 0 PARENT_SCOPE)
+set(FLUTTER_VERSION_PATCH 0 PARENT_SCOPE)
+set(FLUTTER_VERSION_BUILD 1 PARENT_SCOPE)
+
+# Environment variables to pass to tool_backend.sh
+list(APPEND FLUTTER_TOOL_ENVIRONMENT
+  "FLUTTER_ROOT=C:\\flutter"
+  "PROJECT_DIR=D:\\flutter\\web_socket"
+  "FLUTTER_ROOT=C:\\flutter"
+  "FLUTTER_EPHEMERAL_DIR=D:\\flutter\\web_socket\\windows\\flutter\\ephemeral"
+  "PROJECT_DIR=D:\\flutter\\web_socket"
+  "FLUTTER_TARGET=D:\\flutter\\web_socket\\lib\\main.dart"
+  "DART_DEFINES=RkxVVFRFUl9XRUJfQVVUT19ERVRFQ1Q9dHJ1ZQ==,RkxVVFRFUl9XRUJfQ0FOVkFTS0lUX1VSTD1odHRwczovL3d3dy5nc3RhdGljLmNvbS9mbHV0dGVyLWNhbnZhc2tpdC9iOGQzNTgxMGU5MWFiOGZjMzliYTVlN2E0MWJmZjZmNjk3ZThlM2E4Lw=="
+  "DART_OBFUSCATION=false"
+  "TRACK_WIDGET_CREATION=true"
+  "TREE_SHAKE_ICONS=false"
+  "PACKAGE_CONFIG=D:\\flutter\\web_socket\\.dart_tool\\package_config.json"
+)
